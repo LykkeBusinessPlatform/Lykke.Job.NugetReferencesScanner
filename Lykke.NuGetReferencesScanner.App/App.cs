0fd65750ba14ed7de4400cfc5e9056027ffbad2f
Gennadiy Ponomarev <ponomarev@tochka.com>
2020-07-21 03:02:39 +0500

    Add console app Lykke.NuGetReferencesScanner.App

---
+++ Lykke.NuGetReferencesScanner.App/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Lykke.NuGetReferencesScanner.App
+{
+    public class App : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Start nuget scanner");
+            
+            Console.WriteLine("Stop nuget scanner");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lykke.NuGetReferencesScanner.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Lykke.NuGetReferencesScanner.App
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostingContext, services) => services.AddHostedService<App>())
+                .Build();
+
+            await host.StartAsync();
+        }
+    }
+}
