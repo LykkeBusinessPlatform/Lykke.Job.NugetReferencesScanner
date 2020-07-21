1278835e79d7227245bfc8dad7b1df0832c686d5
Gennadiy Ponomarev <ponomarev@tochka.com>
2020-07-21 11:08:25 +0500

    Register core services in DI

--- Lykke.NuGetReferencesScanner.App/App.cs
+++ Lykke.NuGetReferencesScanner.App/App.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Lykke.NuGetReferencesScanner.Domain;
 using Microsoft.Extensions.Hosting;
 
 namespace Lykke.NuGetReferencesScanner.App
 {
     public class App : IHostedService
     {
+        private readonly IReferencesScanner _scanner;
+
+        public App(IReferencesScanner scanner)
+        {
+            _scanner = scanner;
+        }
+
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             Console.WriteLine("Start nuget scanner");
-            
+            var result = await _scanner.GetScanResult();
             Console.WriteLine("Stop nuget scanner");
         }
 
--- Lykke.NuGetReferencesScanner.App/Program.cs
+++ Lykke.NuGetReferencesScanner.App/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Lykke.NuGetReferencesScanner.Domain;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,10 +11,25 @@ namespace Lykke.NuGetReferencesScanner.App
         static async Task Main(string[] args)
         {
             using var host = Host.CreateDefaultBuilder()
-                .ConfigureServices((hostingContext, services) => services.AddHostedService<App>())
+                .ConfigureServices((hostingContext, services) =>
+                {
+                    if(ScannerConfigured(GitHubScanner.OrganizationKeyEnvVar, hostingContext.Configuration))
+                        services.AddSingleton<IOrganizationScanner, GitHubScanner>();
+                    if(ScannerConfigured(BitBucketScanner.AccountEnvVar, hostingContext.Configuration))
+                        services.AddSingleton<IOrganizationScanner, BitBucketScanner>();
+                    
+                    services.AddSingleton<IReferencesScanner, GitScanner>();
+                    services.AddHostedService<App>();
+                })
                 .Build();
 
             await host.StartAsync();
+        }
+
+        private static bool ScannerConfigured(string scannerEnvVar, IConfiguration configuration)
+        {
+             var key = configuration[scannerEnvVar];
+             return !string.IsNullOrWhiteSpace(key);
         }
     }
 }
